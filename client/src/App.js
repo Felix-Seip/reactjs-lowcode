8fa084571e0146e72ea454c183918fe1c5355a33
Felix Seip <felixseip01@gmail.com>
2021-02-20 13:12:54 +0100

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import "./scss/theme.default.scss";
+
+function App() {
+  return (
+    <div className="row">
+      <section className="py-5 px-5 mx-auto">
+        <h1 className="hero-heading mb-0">Get started here!</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
